089013fa9c1bce46d9f67816fb7603db0f140c39
GudniNathan <1493259+GudniNathan@users.noreply.github.com>
2024-02-24 19:14:36 -0500

    update

--- install.bash
+++ install.bash
@@ -1,6 +1,7 @@
 sudo apt-get update ; sudo apt-get install htop dstat python3-pip
 pip3 install torch torchvision
-pip3 install tqdm
+pip3 install tqdm boto3 requests regex sentencepiece sacremoses
+pip3 install pytorch-transformers 
 
 cd $HOME
 mkdir glue_data
