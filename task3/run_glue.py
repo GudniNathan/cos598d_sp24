17f42f19299ac03c1230ef01ca814da653a8d159
GudniNathan <1493259+GudniNathan@users.noreply.github.com>
2024-02-25 16:07:47 -0500

    update

--- task3/run_glue.py
+++ task3/run_glue.py
@@ -432,7 +432,7 @@ def main():
         config=config
     )
     ddp_model = DDP(model)
-    logger.info("Model loaded.")
+    print("Model loaded.")
     ##################################################
 
     if args.local_rank == 0:
