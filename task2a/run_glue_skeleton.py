5d150c8a562b45048a8dd5c6ca252ccc2073a46a
GudniNathan <1493259+GudniNathan@users.noreply.github.com>
2024-02-24 20:34:28 -0500

    update

--- task2a/run_glue_skeleton.py
+++ task2a/run_glue_skeleton.py
@@ -187,7 +187,8 @@ def train(args, train_dataset, model, tokenizer):
         
         ##################################################
         # TODO(cos598d): call evaluate() here to get the model performance after every epoch.
-        results = evaluate(args, model, tokenizer)
+        if args.local_rank in [-1, 0] and args.do_eval:
+            results = evaluate(args, model, tokenizer)
         ##################################################
 
     return global_step, tr_loss / global_step
