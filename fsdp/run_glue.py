4508ec3ea13f5bac46127ac4f54a3492a4afd409
GudniNathan <1493259+GudniNathan@users.noreply.github.com>
2024-04-29 19:14:57 -0400

    cpu offload

--- fsdp/run_glue.py
+++ fsdp/run_glue.py
@@ -106,10 +106,10 @@ def train(args, train_dataset, model, tokenizer):
     
     fsdp_model = FSDP(
         model,
-        # cpu_offload=CPUOffload(True),
+        cpu_offload=CPUOffload(True),
         auto_wrap_policy=size_based_auto_wrap_policy,
         # backward_prefetch=BackwardPrefetch.BACKWARD_PRE,
-        # sharding_strategy=ShardingStrategy.FULL_SHARD,
+        sharding_strategy=ShardingStrategy.FULL_SHARD,
         device_id=args.local_rank,
     )
     
