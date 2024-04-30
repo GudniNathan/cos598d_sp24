9460ee4d657b2a48c1f7d5336e2797f14e49b89a
GudniNathan <1493259+GudniNathan@users.noreply.github.com>
2024-04-29 23:20:09 -0400

    update

--- task3/execute.bash
+++ task3/execute.bash
@@ -18,7 +18,6 @@ torchrun --nnodes 1 --nproc_per_node 4 run_glue.py \
   --num_train_epochs 10 \
   --output_dir /tmp/$TASK_NAME/ \
   --overwrite_output_dir \
-  --local_rank $LOCAL_RANK \
   --master_addr $MASTER_ADDR \
   --master_port $MASTER_PORT \
   --world_size $WORLD_SIZE
