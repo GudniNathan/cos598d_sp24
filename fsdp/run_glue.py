00a180f4391674010065532eaca79bf22a43cf6b
GudniNathan <1493259+GudniNathan@users.noreply.github.com>
2024-04-29 23:13:51 -0400

    hmmm

--- fsdp/run_glue.py
+++ fsdp/run_glue.py
@@ -129,7 +129,7 @@ def fsdp_main(args, train_dataset, eval_dataset, model, tokenizer):
         backward_prefetch=BackwardPrefetch.BACKWARD_POST,
         sharding_strategy=ShardingStrategy.FULL_SHARD,
         # device_id=args.local_rank,
-        sync_module_states=True,
+        # sync_module_states=True,
     )
     
     # Print the model architecture to see how the model is sharded
