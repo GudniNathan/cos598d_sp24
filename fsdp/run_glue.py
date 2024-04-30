4bef9aa996a14dc22d3b0506444e778a32292343
GudniNathan <1493259+GudniNathan@users.noreply.github.com>
2024-04-29 20:56:48 -0400

    vscode autosave

--- fsdp/run_glue.py
+++ fsdp/run_glue.py
@@ -118,10 +118,10 @@ def train(args, train_dataset, model, tokenizer):
         model,
         cpu_offload=CPUOffload(True),
         auto_wrap_policy=my_auto_wrap_policy,
-        # backward_prefetch=BackwardPrefetch.BACKWARD_POST,
-        # sharding_strategy=ShardingStrategy.SHARD_GRAD_OP,
+        backward_prefetch=BackwardPrefetch.BACKWARD_POST,
+        sharding_strategy=ShardingStrategy.SHARD_GRAD_OP,
         device_id=args.local_rank,
-        # sync_module_states=True,
+        sync_module_states=True,
     )
     
     # Print the model architecture to see how the model is sharded
