beb8c37205d54eeda40ce57053d8b6806435006c
GudniNathan <1493259+GudniNathan@users.noreply.github.com>
2024-04-29 20:30:38 -0400

    vscode autosave

--- fsdp/run_glue.py
+++ fsdp/run_glue.py
@@ -112,7 +112,7 @@ def train(args, train_dataset, model, tokenizer):
     fsdp_model = FSDP(
         model,
         cpu_offload=CPUOffload(True),
-        auto_wrap_policy=transformer_auto_wrap_policy,
+        auto_wrap_policy=my_auto_wrap_policy,
         backward_prefetch=BackwardPrefetch.BACKWARD_POST,
         sharding_strategy=ShardingStrategy.FULL_SHARD,
         device_id=args.local_rank,
