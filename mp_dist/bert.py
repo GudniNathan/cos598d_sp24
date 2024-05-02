a216861b8b4c8f58ca8cb204a97786abcab29f74
GudniNathan <1493259+GudniNathan@users.noreply.github.com>
2024-05-01 22:15:28 -0400

    balance gpus a bit better

--- mp_dist/bert.py
+++ mp_dist/bert.py
@@ -13,6 +13,7 @@ from torch import nn
 import os
 
 import torch
+import torch.distributed
 
 # Model parallel version of BertForSequenceClassification
 # This is the same as the original BertForSequenceClassification,
@@ -47,7 +48,7 @@ class BertEncoderMP(BertEncoder):
         
         self.gpu_allocation = [0] * config.num_hidden_layers
         for i, layer in enumerate(self.layer):
-            gpu = (i+1) // layer_count
+            gpu = i // layer_count
             layers[i] = layer.to(f"cuda:{gpu}")
             self.gpu_allocation[i] = gpu
         self.gpu_allocation.append(gpu) # For the output layer
@@ -83,3 +84,68 @@ class BertEncoderMP(BertEncoder):
             outputs = outputs + (all_attentions,)
             
         return outputs  # last-layer hidden state, (all hidden states), (all attentions)
+
+class BertEncoderMPDistributed(BertEncoder):
+    def __init__(self, config):
+        super().__init__(config)
+        
+        self.rank = torch.distributed.get_rank()
+        self.world_size = torch.distributed.get_world_size()
+        
+        # This instance will only hold the layers assigned to this rank.
+        # The layers are divided among the ranks.
+        # For example, if there are 12 layers and 4 ranks,
+        # rank 0 will hold layers 0, 1, 2
+        # rank 1 will hold layers 3, 4, 5
+        # rank 2 will hold layers 6, 7, 8
+        # rank 3 will hold layers 9, 10, 11
+        self.gpu_allocation = [0] * config.num_hidden_layers
+        layer_count = config.num_hidden_layers // world_size
+        layers = []
+        for i in range(config.num_hidden_layers):
+            gpu = (i+1) // layer_count
+            self.gpu_allocation[i] = gpu
+            if gpu == rank:
+                layers.append(BertLayer(config))
+                
+        self.layer = nn.ModuleList(layers)
+
+    def forward(self, hidden_states, attention_mask, head_mask=None):
+        all_hidden_states = ()
+        all_attentions = ()
+        
+        if self.rank != 0:
+            torch.distributed.recv(tensor=hidden_states, src=self.rank-1)
+        
+        for i, layer_module in enumerate(self.layer):
+            if self.output_hidden_states:
+                all_hidden_states = all_hidden_states + (hidden_states,)
+
+            layer_outputs = layer_module(hidden_states, attention_mask, head_mask[i])
+            hidden_states = layer_outputs[0]
+
+            if self.output_attentions:
+                all_attentions = all_attentions + (layer_outputs[1],)
+        
+        # Send the hidden states to the next rank
+        if self.rank == self.world_size - 1:
+            torch.distributed.send(tensor=hidden_states, dst=self.rank+1)            
+        else:
+            # Last rank sends the hidden states back to the first rank
+            torch.distributed.send(tensor=hidden_states, dst=0)
+            
+        # Receive the final hidden states from the last rank
+        if self.rank == 0:
+            torch.distributed.recv(tensor=hidden_states, src=self.world_size-1)
+        
+        # Add last layer
+        if self.output_hidden_states:
+            all_hidden_states = all_hidden_states + (hidden_states,)
+
+        outputs = (hidden_states,)
+        if self.output_hidden_states:
+            outputs = outputs + (all_hidden_states,)
+        if self.output_attentions:
+            outputs = outputs + (all_attentions,)
+        return outputs  # last-layer hidden state, (all hidden states), (all attentions)
+    
