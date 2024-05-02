e80479582568688371cd76a5a1273236efc37395
GudniNathan <1493259+GudniNathan@users.noreply.github.com>
2024-05-01 20:42:59 -0400

    ??

--- mp/bert.py
+++ mp/bert.py
@@ -58,6 +58,11 @@ class BertEncoderMP(BertEncoder):
         # Print the devices
         hidden_state_device = hidden_states.device
         print(f"Hidden states device: {hidden_state_device}")
+        attention_mask_device = attention_mask.device
+        print(f"Attention mask device: {attention_mask_device}")
+        if head_mask is not None:
+            head_mask_device = head_mask.device
+            print(f"Head mask device: {head_mask_device}")
         for i, layer_module in enumerate(self.layer):
             if self.output_hidden_states:
                 all_hidden_states = all_hidden_states + (hidden_states,)
