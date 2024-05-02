d00b922ace0a62f99558cef0f6c9eb0c54c004d8
GudniNathan <1493259+GudniNathan@users.noreply.github.com>
2024-05-02 00:58:23 -0400

    cutlass added?

--- install.bash
+++ install.bash
@@ -1,5 +1,6 @@
 apt-get update
 apt-get -y install htop dstat python3-pip
+apt-get -y install libaio-dev
 
 # Install PyTorch and other dependencies
 pip3 uninstall --y torch torchvision torchaudio clean-fid xformers
@@ -12,8 +13,11 @@ pip3 install scipy
 pip3 install scikit-learn
 pip3 install deepspeed
 
-# Download the GLUE data
 cd $HOME
+git clone https://github.com/NVIDIA/cutlass cutlass
+export CUTLASS_PATH=$HOME/cutlass
+
+# Download the GLUE data
 mkdir glue_data
 cd cos598d_sp24
 python3 download_glue_data.py --data_dir $HOME/glue_data
