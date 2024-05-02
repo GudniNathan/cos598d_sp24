ad3c929ef488ca868862ad8f51496fbea2800b32
GudniNathan <1493259+GudniNathan@users.noreply.github.com>
2024-05-02 01:47:34 -0400

    simplify with a new image

--- install.bash
+++ install.bash
@@ -2,20 +2,12 @@ apt-get update
 apt-get -y install htop dstat python3-pip
 apt-get -y install libaio-dev
 
-# Install PyTorch and other dependencies
-pip3 uninstall --y torch torchvision torchaudio clean-fid xformers
-pip3 install torch torchvision torchaudio clean-fid xformers
-
 pip3 install torch torchvision
 pip3 install tqdm boto3 requests regex sentencepiece sacremoses
 pip3 install pytorch-transformers 
 pip3 install scipy
 pip3 install scikit-learn
 pip3 install deepspeed
-
-pip3 --y uninstall triton
-pip install triton==1.0.0
-
 
 cd $HOME
 git clone https://github.com/NVIDIA/cutlass $HOME/cutlass
