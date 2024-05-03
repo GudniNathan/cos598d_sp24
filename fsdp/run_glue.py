f9ed1073cfecd35012333735ea2b91e2242588b2
GudniNathan <1493259+GudniNathan@users.noreply.github.com>
2024-05-02 20:12:14 -0400

    test SGD optimizer over ADAM

--- fsdp/run_glue.py
+++ fsdp/run_glue.py
@@ -157,7 +157,7 @@ def fsdp_main(args, train_dataset, eval_dataset, model, tokenizer):
         {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': args.weight_decay},
         {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
         ]
-    optimizer = AdamW(model.parameters(), lr=args.learning_rate, eps=args.adam_epsilon)
+    optimizer = torch.optim.SGD(optimizer_grouped_parameters) #AdamW(model.parameters(), lr=args.learning_rate, eps=args.adam_epsilon)
     scheduler = WarmupLinearSchedule(optimizer, warmup_steps=args.warmup_steps, t_total=t_total)
     if args.fp16:
         try:
