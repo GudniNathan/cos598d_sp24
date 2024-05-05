d5409061f9f0bbc5239bb8560305a547d814f155
GudniNathan <1493259+GudniNathan@users.noreply.github.com>
2024-05-04 20:59:59 -0400

    actually fix

--- pipeline/train_utils.py
+++ pipeline/train_utils.py
@@ -3,7 +3,8 @@ import torch
 import torch.distributed as dist
 from datetime import datetime
 import tqdm
-from pytorch_transformers import BertTokenizer, BertForSequenceClassification
+from pytorch_transformers import BertTokenizer, BertForMaskedLM
+from bert import BertForSequenceClassificationMP
 
 g_gigabyte = 1024**3
 
@@ -99,6 +100,6 @@ def validation(model, rank, world_size, val_loader):
 
 
 def setup_model(model_name):
-        model = BertForSequenceClassification.from_pretrained(model_name)
+        model = BertForSequenceClassificationMP.from_pretrained(model_name)
         tokenizer =  BertTokenizer.from_pretrained(model_name)
         return model, tokenizer
