4ce8515b5464e6f87086adc593512bd4b9b29755
GudniNathan <1493259+GudniNathan@users.noreply.github.com>
2024-05-06 05:17:12 -0400

    ...

--- deepspeed-zero/run_glue.py
+++ deepspeed-zero/run_glue.py
@@ -568,5 +568,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
     args.eval_batch_size = args.per_gpu_eval_batch_size
     BertArgs[0] = args
+    args.profile = True
 
     main(args)
